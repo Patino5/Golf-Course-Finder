36bcd5aba2dc0fcb7e980eb6f69b840f5e92ddcb
Patino5 <106916823+Patino5@users.noreply.github.com>
2023-10-25 02:24:15 +1000

    added basic styling and layout for page

--- main.js
+++ main.js
@@ -24,3 +24,13 @@ let polygon = L.polygon([
     fillOpacity: 0.3,
 }).addTo(map)
 
+marker.bindPopup('<b>Hello world!</b><br>I am a popup.').openPopup();
+circle.bindPopup('I am a circle.')
+polygon.bindPopup('I am a polygon')
+
+const popup = L.popup().setLatLng([51.513, -0.09]).setContent('I am a standalone popup.').openOn(map)
+
+function onMapClick(e) {
+    alert(`You clicked the map at ${e.latlng}` )
+}
+map.on('click', onMapClick)
